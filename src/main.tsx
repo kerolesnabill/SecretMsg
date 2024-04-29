50d044223498468a0495c8db4056ea0f22d19d6d
Kero <keronabilzaki@gmail.com>
2024-04-29 23:13:24 +0300

    Add login page.

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import Signup from "./routes/Signup.tsx";
+import Login from "./routes/Login.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/signup", element: <Signup /> }],
+    children: [
+      { path: "/signup", element: <Signup /> },
+      { path: "/login", element: <Login /> },
+    ],
   },
 ]);
 
---
+++ src/routes/Login.tsx
@@ -0,0 +1,86 @@
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../api";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const [error, setError] = useState("");
+  const [isLoding, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+
+    await api
+      .post("/login", {
+        email: emailRef.current?.value,
+        password: passwordRef.current?.value,
+      })
+      .then((res) => {
+        localStorage.setItem("token", res.data);
+        navigate("/");
+      })
+      .catch((err) => setError(err?.response?.data || "Unexpected error."))
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <div className="card w-full max-w-sm shadow-lg bg-base-100 mx-auto mt-20">
+      <div className="card-header text-center">
+        <h1 className="text-xl font-bold">Login</h1>
+      </div>
+      <div className="card-body">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              className="input input-bordered"
+              maxLength={255}
+              required
+              ref={emailRef}
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="input input-bordered"
+              required
+              ref={passwordRef}
+            />
+          </div>
+          <p className="mt-4 text-error">{error}</p>
+          <div className="form-control mt-4">
+            <button
+              type="submit"
+              className="btn btn-base-200 disabled:disabled"
+              disabled={isLoding}
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <p className="mt-4 text-center">
+          You don't have an account{" "}
+          <Link className="link link-info" to="/signup">
+            Signup
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
