9b70cf2a829ded72f199cd57bc9f51dbe64de593
Kero <keronabilzaki@gmail.com>
2024-05-04 11:58:42 +0300

    Edit Navbar and add log out.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,12 @@ import UserContext from "../context/UserContext";
 const Navbar = () => {
   const ctx = useContext(UserContext);
 
+  const handleSignout = () => {
+    localStorage.clear();
+    ctx?.setUser(null);
+    window.location.assign("/");
+  };
+
   return (
     <div className="navbar bg-base-100 border border-b-amber-900 md:px-20 lg:px-40">
       <div className="flex-1">
@@ -15,8 +21,25 @@ const Navbar = () => {
         </a>
       </div>
       {ctx?.user ? (
-        <div className="navbar-end gap-2">
-          <p className="text-sm  sm:text-lg">Hi, {ctx.user.name}</p>
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <details>
+                <summary>Menu</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/settings">Settings</Link>
+                  </li>
+                  <li>
+                    <button onClick={() => handleSignout()}>Log out</button>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
         </div>
       ) : (
         <div className="navbar-end gap-2">
